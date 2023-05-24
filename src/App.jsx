a2875d9af75f510be0790bddc909eb0ee3e2d5f1
MWaqasMughal <waqaskhalil7785@gmail.com>
2023-05-24 23:16:23 +0500

    Learned useState, reconsilation and if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
+
+  //let a = 90
+
+  let [num, setNum] = useState(90)
+  // write variable and name of variable with Set
+  // useState is the funtion which return variable name and returned function which update the value.
+  let handleIncrement = ()=>{
+      //if(num>=100)return;
+      setNum(num+1)
+      // document.getElementById('a').innerText = a     Manully updating in actual DOM
+  }
+  let handleDecrement = ()=>{
+    //if(num>=80)return;
+    setNum(num-1)
+    // document.getElementById('a').innerText = a     Manully updating in actual DOM
+}
+  // comparing virtual and actual DOM and then comparing both, after that change in the actual DOM from virtual DOM is called reconsilation.
   return (
     <>
-    <Header/>
+    {/* <Header/>
     <Main/>
-    <Footer/>
+    <Footer/> */}
+
+  <div>
+    <p>{num}</p>
+
+    <button disabled={num>=100? true:false} onClick={handleIncrement}>+</button> 
+    <br></br>
+    <button disabled={num<=80? true:false} onClick={handleDecrement}>-</button>
+    </div>
     </>  
+ /* num>100? console.log('The number is greated than 100') : console.log('The number is less than 100') ... 
+        we can use this statement instead of if else statement*/
+    
   );
+
+  
 }
 
 export default App;
